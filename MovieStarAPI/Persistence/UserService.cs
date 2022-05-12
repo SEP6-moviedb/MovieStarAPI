18a2c894eed531efebb707375b6437227cf455c3
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-05-12 13:14:19 +0200

    user dummydata

--- MovieStarAPI/Persistence/UserService.cs
+++ MovieStarAPI/Persistence/UserService.cs
@@ -7,8 +7,20 @@ namespace MovieStarAPI.Persistence
         public static async Task<HttpStatusCode> GetUser(string? username, string? password)
         {
             Console.WriteLine("username: " + username + " password: " + password);
-            return HttpStatusCode.OK;
-            //return HttpStatusCode.NotFound;
+
+            var userPasswordDictionary = new Dictionary<string, string>();
+
+            userPasswordDictionary.Add("anders@email.dk", "anders1234");
+            userPasswordDictionary.Add("bo@email.dk", "bo1234");
+            userPasswordDictionary.Add("carl@email.dk", "carl1234");
+
+            string found; 
+            if (userPasswordDictionary.TryGetValue(username, out found) && found == password)
+            {
+                // key/value pair exists
+                return HttpStatusCode.OK;
+            }
+                return HttpStatusCode.NotFound;
         }
     }
 }
