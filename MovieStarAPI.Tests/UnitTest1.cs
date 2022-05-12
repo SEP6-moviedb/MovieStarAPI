f888f6c3823402a308755f45e1fa5ee74dd320af
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-05-12 10:12:22 +0200

    NUnit Test Project added

---
+++ MovieStarAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MovieStarAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
