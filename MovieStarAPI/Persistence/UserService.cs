fa0274e35f58203c20e4e4456161b5b0eaa9593e
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-05-12 12:54:30 +0200

    Endpoint "/users" prototype.  Returns statuscode 200 for any username + password.

---
+++ MovieStarAPI/Persistence/UserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace MovieStarAPI.Persistence
+{
+    public class UserService
+    {
+        public static async Task<HttpStatusCode> GetUser(string? username, string? password)
+        {
+            Console.WriteLine("username: " + username + " password: " + password);
+            return HttpStatusCode.OK;
+            //return HttpStatusCode.NotFound;
+        }
+    }
+}
