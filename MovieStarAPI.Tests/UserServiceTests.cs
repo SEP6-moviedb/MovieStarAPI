760136d3fd6cc4f4ad33275518179ff3d39bebfd
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-05-19 14:06:40 +0200

    UserServiceTests updated

--- MovieStarAPI.Tests/UserServiceTests.cs
+++ MovieStarAPI.Tests/UserServiceTests.cs
@@ -13,22 +13,22 @@ namespace MovieStarAPI.Tests
         [Test]
         public async Task GET_existing_user_with_valid_credentials_receives_200_OK()
         {
-            Microsoft.AspNetCore.Mvc.StatusCodeResult? statusCodeResult = await UserService.GetUser("anders@email.dk", "anders1234");
-            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
+            Microsoft.AspNetCore.Mvc.ContentResult? contentResult = await UserService.GetUser("anders@email.dk", "anders1234");
+            Assert.That(contentResult.StatusCode, Is.EqualTo(200));
         }
 
         [Test]
         public async Task GET_non_existing_user_receives_401_Unauthorized()
         {
-            Microsoft.AspNetCore.Mvc.StatusCodeResult? statusCodeResult = await UserService.GetUser("noone@email.dk", "carl1234");
-            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(401));
+            Microsoft.AspNetCore.Mvc.ContentResult? contentResult = await UserService.GetUser("noone@email.dk", "carl1234");
+            Assert.That(contentResult.StatusCode, Is.EqualTo(401));
         }
 
         [Test]
         public async Task GET_existing_user_with_wrong_password_receives_401_Unauthorized()
         {
-            Microsoft.AspNetCore.Mvc.StatusCodeResult? statusCodeResult = await UserService.GetUser("carl@email.dk", "wrongpassword");
-            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(401));
+            Microsoft.AspNetCore.Mvc.ContentResult? contentResult = await UserService.GetUser("carl@email.dk", "wrongpassword");
+            Assert.That(contentResult.StatusCode, Is.EqualTo(401));
         }
     }
 }
