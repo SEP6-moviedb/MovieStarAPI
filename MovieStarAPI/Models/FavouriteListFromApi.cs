6cf6d4e531fab6e5ea75012a273ef9f5d5f45b05
Sockom <57130573+Sockom@users.noreply.github.com>
2022-05-20 14:11:58 +0200

    Favourites api calls works

---
+++ MovieStarAPI/Models/FavouriteListFromApi.cs
@@ -0,0 +1,29 @@
+﻿namespace MovieStarAPI.Models; 
+
+public class FavouriteRoot {
+    public string _id { get; set; }
+    public Favourite favourite { get; set; }
+
+
+}
+public class Favourite
+{
+
+    public string movieid { get; set; }
+    public string userid { get; set; }
+    public string moviename { get; set; }
+
+    public Favourite(string movieId, string userid, string moviename)
+    {
+            
+        this.movieid = movieId;
+        this.userid = userid;
+        this.moviename = moviename;
+
+    }
+
+    public override string ToString()
+    {
+        return "Movie ID: " + movieid + ", \t User ID: " + userid + ", \t Movie name: " + moviename;
+    }
+}
